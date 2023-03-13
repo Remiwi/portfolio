6ed92d63034e9cfa438cdc53dc56cb87cbb2d8f9
Remi <70982674+Remi-NYU@users.noreply.github.com>
2023-03-13 13:35:23 -0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import styleImport from 'vite-plugin-style-import';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+  ]
+});
