73d272445fb7a84ce4c04d34dd7bd1507203b512
Remi <70982674+Remi-NYU@users.noreply.github.com>
2024-01-05 12:28:21 -0300

    Added contact icons

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -1,9 +1,38 @@
+import Image from "next/image";
+
 export default function Contacts({ className }: { className?: string }) {
   return (
     <div className={className}>
-      <a href="#">GitHub</a>
-      <a href="#">LinkedIn</a>
-      <a href="#">Contact me</a>
+      <div className="flex flex-row items-center justify-center gap-1">
+        <Image
+          src="/icons/github.png"
+          alt="Github Icon"
+          width={100}
+          height={100}
+          className="w-6 h-6"
+        />
+        <a href="#">GitHub</a>
+      </div>
+      <div className="flex flex-row items-center justify-center gap-1">
+        <Image
+          src="/icons/linkedin.png"
+          alt="LinkedIn Icon"
+          width={100}
+          height={100}
+          className="w-6 h-6"
+        />
+        <a href="#">LinkedIn</a>
+      </div>
+      <div className="flex flex-row items-center justify-center gap-1">
+        <Image
+          src="/icons/email.png"
+          alt="Email Icon"
+          width={100}
+          height={100}
+          className="w-6 h-6"
+        />
+        <a href="#">Email</a>
+      </div>
     </div>
   );
 }
