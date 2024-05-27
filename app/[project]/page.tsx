6be773f1e411919718fcff21998686289b9a0a37
Remi <70982674+Remi-NYU@users.noreply.github.com>
2024-05-27 11:26:56 -0400

    Test 3

--- app/[project]/page.tsx
+++ app/[project]/page.tsx
@@ -1,26 +1,18 @@
-// import { promises as fs } from "fs";
-// import ProjectMarkdown from "@/components/ProjectMarkdown";
-
-// export default async function Page({
-//   params,
-// }: {
-//   params: { project: string };
-// }) {
-//   try {
-//     const file_contents = await fs.readFile(
-//       process.cwd() + `/markdown/${params.project}.md`,
-//       "utf8"
-//     );
-//     return <ProjectMarkdown>{file_contents}</ProjectMarkdown>;
-//   } catch (e: any) {
-//     return <h1>404 - Page not found: {e}</h1>;
-//   }
-// }
+import { promises as fs } from "fs";
+import ProjectMarkdown from "@/components/ProjectMarkdown";
 
 export default async function Page({
   params,
 }: {
   params: { project: string };
 }) {
-  return <h1>404 - Page not found</h1>;
+  try {
+    const file_contents = await fs.readFile(
+      process.cwd() + `/markdown/${params.project}.md`,
+      "utf8"
+    );
+    return <ProjectMarkdown>{file_contents}</ProjectMarkdown>;
+  } catch (e: any) {
+    return <h1>404 - Page not found: {e.toString()}</h1>;
+  }
 }
