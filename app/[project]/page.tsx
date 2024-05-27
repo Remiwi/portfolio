b5e2baf5a232986b411599fc59ea16061d395861
Remi <70982674+Remi-NYU@users.noreply.github.com>
2024-05-27 11:57:40 -0400

    Undo last thing

--- app/[project]/page.tsx
+++ app/[project]/page.tsx
@@ -1,7 +1,4 @@
 import { promises as fs } from "fs";
-import path from "path";
-import getConfig from "next/config";
-const { serverRuntimeConfig } = getConfig();
 import ProjectMarkdown from "@/components/ProjectMarkdown";
 
 export default async function Page({
@@ -11,10 +8,7 @@ export default async function Page({
 }) {
   try {
     const file_contents = await fs.readFile(
-      path.join(
-        serverRuntimeConfig.PROJECT_ROOT,
-        `/public/markdown/${params.project}.md`
-      ),
+      process.cwd() + `/public/markdown/${params.project}.md`,
       "utf8"
     );
     return <ProjectMarkdown>{file_contents}</ProjectMarkdown>;
